b18ff526bf6fb87d39ed74ab281df87b4a7fe782
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-10-18 15:05:18 +0700

    model

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,10 @@ router.get('/booking', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'booking.html'));
 });
 
+router.get('/Login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'LoginAndRegister.html'));
+});
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,15 +5,7 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/homepage', (req, res) => {
-    res.render('page1.ejs')
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
